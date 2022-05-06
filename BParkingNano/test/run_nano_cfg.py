fb6835030ce0489eb9904b4ff6b94e917b64060a
bainbrid <bainbrid@users.noreply.github.com>
2022-05-06 16:31:08 +0200

    process 2018 Charmonium PD using nanoAOD_KMuMu_step

--- BParkingNano/test/run_nano_cfg.py
+++ BParkingNano/test/run_nano_cfg.py
@@ -35,7 +35,7 @@ options.register('skip', 0,
 )
 
 options.setDefault('maxEvents', 100)
-options.setDefault('tag', '10215')
+options.setDefault('tag', '124X')
 options.parseArguments()
 
 globaltag = '102X_dataRun2_v11' if not options.isMC else '102X_upgrade2018_realistic_v15'
@@ -46,8 +46,17 @@ extension = {False : 'data', True : 'mc'}
 outputFileNANO = cms.untracked.string('_'.join(['BParkNANO', extension[options.isMC], options.tag])+'.root')
 outputFileFEVT = cms.untracked.string('_'.join(['BParkFullEvt', extension[options.isMC], options.tag])+'.root')
 if not options.inputFiles:
-    options.inputFiles = ['/store/data/Run2018B/ParkingBPH4/MINIAOD/05May2019-v2/230000/6B5A24B1-0E6E-504B-8331-BD899EB60110.root'] if not options.isMC else \
-                         ['/store/cmst3/group/bpark/BToKmumu_1000Events_MINIAOD.root']
+    options.inputFiles = [
+        #'root://cms-xrd-global.cern.ch//store/data/Run2018B/ParkingBPH4/MINIAOD/05May2019-v2/230000/6B5A24B1-0E6E-504B-8331-BD899EB60110.root' # original
+        'root://cms-xrd-global.cern.ch//store/data/Run2018D/Charmonium/MINIAOD/12Nov2019_UL2018-v1/00000/00AC5CFC-5390-F947-911B-2074A7DFF23D.root'
+        #'root://cms-xrd-global.cern.ch//store/user/jodedra/Charmonium_20223003/Charmonium/Spring22_Trigger_20223003_MiniAODHLTRAWCOMB/220330_132012/0000/output_1.root'
+    ] if not options.isMC else [
+        #'file:///eos/cms/store/cmst3/group/bpark/BToKmumu_1000Events_MINIAOD.root' # original
+        #'file:///eos/cms/store/group/phys_bphys/ec/RKR3/test/test_files/MINIAOD_BuToKJpsi_Toee_Skim_1000events_102X.root'
+        #'file:///eos/cms/store/group/phys_bphys/ec/RKR3/test/test_files/MINIAOD_BuToKMuMu_Skim_1000events_102X.root'
+        'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKMuMu_probefilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v2/40000/43F82832-A9A2-5D45-950F-BC7D8DAC9C9B.root'
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15_ext1-v2/100000/041EF22D-69F5-914D-AD60-F2D1187B0842.root'
+    ]
 annotation = '%s nevts:%d' % (outputFileNANO, options.maxEvents)
 
 from Configuration.StandardSequences.Eras import eras
@@ -259,18 +268,18 @@ process.NANOAODoutput_step = cms.EndPath(process.NANOAODoutput)
 # Schedule definition
 process.schedule = cms.Schedule(
                                 process.nanoAOD_KMuMu_step,
-                                process.nanoAOD_Kee_step, 
-                                process.nanoAOD_KstarMuMu_step,
-                                process.nanoAOD_KstarEE_step,
+                                #process.nanoAOD_Kee_step,
+                                #process.nanoAOD_KstarMuMu_step,
+                                #process.nanoAOD_KstarEE_step,
                                 process.endjob_step, 
                                 process.NANOAODoutput_step
                                )
 if options.wantFullRECO:
     process.schedule = cms.Schedule(
                                     process.nanoAOD_KMuMu_step,
-                                    process.nanoAOD_Kee_step, 
-                                    process.nanoAOD_KstarMuMu_step,
-                                    process.nanoAOD_KstarEE_step,
+                                    #process.nanoAOD_Kee_step,
+                                    #process.nanoAOD_KstarMuMu_step,
+                                    #process.nanoAOD_KstarEE_step,
                                     process.endjob_step, 
                                     process.FEVTDEBUGHLToutput_step, 
                                     process.NANOAODoutput_step
@@ -281,9 +290,9 @@ associatePatAlgosToolsTask(process)
 process.NANOAODoutput.SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring(
                                    'nanoAOD_KMuMu_step', 
-                                   'nanoAOD_Kee_step',
-                                   'nanoAOD_KstarMuMu_step',
-                                   'nanoAOD_KstarEE_step',
+                                   #'nanoAOD_Kee_step',
+                                   #'nanoAOD_KstarMuMu_step',
+                                   #'nanoAOD_KstarEE_step',
                                    )
 )
 
