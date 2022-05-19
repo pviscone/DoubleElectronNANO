b187f99c1032c625fee6211060776bf1a1f95c70
bainbrid <bainbrid@users.noreply.github.com>
2022-05-19 14:42:40 +0200

    organise input files

--- BParkingNano/test/run_nano_cfg.py
+++ BParkingNano/test/run_nano_cfg.py
@@ -42,21 +42,39 @@ globaltag = '102X_dataRun2_v11' if not options.isMC else '102X_upgrade2018_reali
 if options._beenSet['globalTag']:
     globaltag = options.globalTag
 
+filesDict = {
+    'data':{
+        # original test file (BParking 2018)
+        'test_orig':'root://cms-xrd-global.cern.ch//store/data/Run2018B/ParkingBPH4/MINIAOD/05May2019-v2/230000/6B5A24B1-0E6E-504B-8331-BD899EB60110.root',
+        # example (default) charmomium file
+        'charmonium_orig':'root://cms-xrd-global.cern.ch//store/data/Run2018D/Charmonium/MINIAOD/12Nov2019_UL2018-v1/00000/00AC5CFC-5390-F947-911B-2074A7DFF23D.root',
+        # example charmomium file with emulated di-electron trigger
+        'charmonium_jay':'root://cms-xrd-global.cern.ch//store/user/jodedra/Charmonium_20221705/Charmonium/Spring22_Trigger_20221705_MiniAODHLTRAWCOMB/220517_091036/0000/output_10.root',
+    },
+    'mc':{
+        # original test file
+        'test_orig':'file:///eos/cms/store/cmst3/group/bpark/BToKmumu_1000Events_MINIAOD.root',
+        # test BuToKJpsi_Toee file
+        'test_BuToKJpsi_Toee':'file:///eos/cms/store/group/phys_bphys/ec/RKR3/test/test_files/MINIAOD_BuToKJpsi_Toee_Skim_1000events_102X.root',
+        # test BuToKMuMu file
+        'test_BuToKMuMu':'file:///eos/cms/store/group/phys_bphys/ec/RKR3/test/test_files/MINIAOD_BuToKMuMu_Skim_1000events_102X.root',
+        # example BuToKMuMu file
+        'BuToKMuMu':'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKMuMu_probefilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v2/40000/43F82832-A9A2-5D45-950F-BC7D8DAC9C9B.root',
+        # example BuToKJpsi_ToMuMu file
+        'BuToKJpsi_ToMuMu':'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKJpsi_ToMuMu_probefilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v2/120000/028A5AEE-90AD-7448-A186-A86AA85E1881.root',
+        # example BuToKJpsi_Toee file
+        'BuToKJpsi_Toee':'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15_ext1-v2/100000/041EF22D-69F5-914D-AD60-F2D1187B0842.root',
+    }
+}
+
 extension = {False : 'data', True : 'mc'}
 outputFileNANO = cms.untracked.string('_'.join(['BParkNANO', extension[options.isMC], options.tag])+'.root')
 outputFileFEVT = cms.untracked.string('_'.join(['BParkFullEvt', extension[options.isMC], options.tag])+'.root')
 if not options.inputFiles:
     options.inputFiles = [
-        #'root://cms-xrd-global.cern.ch//store/data/Run2018B/ParkingBPH4/MINIAOD/05May2019-v2/230000/6B5A24B1-0E6E-504B-8331-BD899EB60110.root' # original
-        'root://cms-xrd-global.cern.ch//store/data/Run2018D/Charmonium/MINIAOD/12Nov2019_UL2018-v1/00000/00AC5CFC-5390-F947-911B-2074A7DFF23D.root'
-        #'root://cms-xrd-global.cern.ch//store/user/jodedra/Charmonium_20223003/Charmonium/Spring22_Trigger_20223003_MiniAODHLTRAWCOMB/220330_132012/0000/output_1.root'
+        filesDict['data']['charmonium_jay']
     ] if not options.isMC else [
-        #'file:///eos/cms/store/cmst3/group/bpark/BToKmumu_1000Events_MINIAOD.root' # original
-        #'file:///eos/cms/store/group/phys_bphys/ec/RKR3/test/test_files/MINIAOD_BuToKJpsi_Toee_Skim_1000events_102X.root'
-        #'file:///eos/cms/store/group/phys_bphys/ec/RKR3/test/test_files/MINIAOD_BuToKMuMu_Skim_1000events_102X.root'
-        #'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKMuMu_probefilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v2/40000/43F82832-A9A2-5D45-950F-BC7D8DAC9C9B.root'
-        'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKJpsi_ToMuMu_probefilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v2/120000/028A5AEE-90AD-7448-A186-A86AA85E1881.root'
-        #'root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18MiniAOD/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15_ext1-v2/100000/041EF22D-69F5-914D-AD60-F2D1187B0842.root'
+        filesDict['mc']['BuToKJpsi_ToMuMu']
     ]
 annotation = '%s nevts:%d' % (outputFileNANO, options.maxEvents)
 
