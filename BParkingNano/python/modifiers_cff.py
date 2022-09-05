be3cfe6fbeadb820c8e9c58b2d32c8a2e0839b6f
bainbrid <bainbrid@users.noreply.github.com>
2022-09-05 16:48:24 +0200

    add DiEle modifier

--- BParkingNano/python/modifiers_cff.py
+++ BParkingNano/python/modifiers_cff.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 BToKMuMu_OpenConfig =  cms.Modifier()
 BToKEE_OpenConfig =  cms.Modifier()
-
+BToKEE_DiEle =  cms.Modifier()
 BToKMuMu_DiMuon =  cms.Modifier()
