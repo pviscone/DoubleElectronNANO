969defac5cc179235a021ae62cf33dfb77b263ad
noepalm <palmeri.1871005@studenti.uniroma1.it>
2025-04-10 17:36:04 +0200

    Added background samples to production

--- BParkingNano/production/run.sh
+++ BParkingNano/production/run.sh
@@ -14,9 +14,19 @@ source /cvmfs/cms.cern.ch/common/crab-setup.sh #needed to make CRABClient work
 # python3 submit_on_crab.py --filter=BuToKJpsi_Toee_2022preEE --yaml=samples_Run3_2022.yml --lhcRun=3 --year=2022
 #python3 submit_on_crab.py --filter=BuToKJpsi_Toee_2022postEE --yaml=samples_Run3_2022.yml --lhcRun=3 --year=2022
 
-## 2023
+### 2023
+## DATA
 # python3 submit_on_crab.py --filter=Run2023C --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
-#python3 submit_on_crab.py --filter=Run2023D --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=Run2023D --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
 
-#python3 submit_on_crab.py --filter=BuToKJpsi_Toee_2023preBPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+## BACKGROUND SAMPLES
+# python3 submit_on_crab.py --filter=BuToKJpsi_Toee_2023preBPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
 # python3 submit_on_crab.py --filter=BuToKJpsi_Toee_2023BPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=JPsiToEE_pth0to10_*_2023preBPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=JPsiToEE_pth0to10_*_2023BPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=JPsiToEE_pth10toInf_*_2023preBPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=JPsiToEE_pth10toInf_*_2023BPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=UpsilonToEE_pth0to10_*_2023preBPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=UpsilonToEE_pth0to10_*_2023BPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=UpsilonToEE_pth10toInf_*_2023preBPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
+# python3 submit_on_crab.py --filter=UpsilonToEE_pth10toInf_*_2023BPix --yaml=samples_Run3_2023.yml --lhcRun=3 --year=2023
