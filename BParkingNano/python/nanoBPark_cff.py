ed88222ce46498f45d51cf101c737b8d20ffa496
Peter Eduard Meiring <peter.eduard.meiring@cern.ch>
2025-09-02 17:15:10 +0200

    fix to tauID for preEE 2022 data

--- BParkingNano/python/nanoBPark_cff.py
+++ BParkingNano/python/nanoBPark_cff.py
@@ -44,6 +44,9 @@ from DoubleElectronNANO.BParkingNano.tracksBPark_cff import *
 ## Dielectron collection
 from DoubleElectronNANO.BParkingNano.dielectron_cff import *
 
+# Removing dependency on byVVVLooseDeepTau2018v2p5VSjet, which doesn't work for 2022 preEE mini (v<4)
+finalTaus.cut=cms.string("pt > 18 && ((tauID('decayModeFindingNewDMs') > 0.5 && (tauID('byLooseCombinedIsolationDeltaBetaCorr3Hits') || (tauID('chargedIsoPtSumdR03')+max(0.,tauID('neutralIsoPtSumdR03')-0.072*tauID('puCorrPtSum'))<2.5))) || (?isTauIDAvailable('byUTagCHSVSjetraw')?tauID('byUTagCHSVSjetraw'):-1) > {} || (?isTauIDAvailable('byUTagPUPPIVSjetraw')?tauID('byUTagPUPPIVSjetraw'):-1) > {})".format(0.05, 0.05))
+
 # nanoSequenceOnlyFullSim = cms.Sequence(triggerObjectBParkTables + l1bits)
 nanoSequenceOnlyFullSim = cms.Sequence(electronTriggerObjectBParkTables + l1bits)
 
